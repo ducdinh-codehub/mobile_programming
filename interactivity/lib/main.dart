853bd5afeba673bf235871212b013ddab3882e94
ducdinh-codehub <dinhduc31197@gmail.com>
2021-11-25 18:41:27 +0700

    Modified interactivity project added parent widget example

--- interactivity/lib/main.dart
+++ interactivity/lib/main.dart
@@ -39,6 +39,72 @@ class _TapBoxAState extends State<TapBoxA>{
     );
   }
 }
+
+////////////ParentWidget////////////
+class ParentWidget extends StatefulWidget{
+    const ParentWidget({Key? key}) : super(key: key);
+
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget>{
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue){
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return SizedBox(
+      child: TapBoxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+////////////TapBoxB////////////
+class TapBoxB extends StatelessWidget{
+  const TapBoxB({
+    Key? key, 
+    this.active = false, 
+    required this.onChanged,}) : super(key: key);
+
+    final bool active;
+    final ValueChanged<bool> onChanged;
+
+    void _handleTap(){
+      onChanged(!active);
+    }
+    @override
+    Widget build(BuildContext context){
+      return GestureDetector(
+        onTap: _handleTap,
+        child: Container(
+          child: Container(
+            child: Center(
+              child: Text(
+                active ? 'Active' : 'Inactive',
+                style: const TextStyle(fontSize: 32.0, color: Colors.white),
+              ),
+            ),
+            width: 200.0,
+            height: 200.0,
+            decoration: BoxDecoration(
+              color: active ? Colors.lightGreen[700] : Colors.grey[600],
+            )
+          ),
+        ),
+      );
+    }
+
+}
+
 ////////////App////////////
 class MyApp extends StatelessWidget{
   const MyApp({Key? key}) : super(key: key);
@@ -50,7 +116,7 @@ class MyApp extends StatelessWidget{
           appBar: AppBar(
             title: const Text('Flutter Demo'),
           ),
-          body: const Center(child: TapBoxA(),
+          body: const Center(child: ParentWidget(),
         ),
       ),
     );
